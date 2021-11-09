d35bb73b3a0dd22fd9215f71341bce55667aaef0
Abdulrahman Alterkait <alterkait@pc-75-89.customer.ask4.lan>
2021-11-09 01:39:06 +0000

    initial commit

---
+++ kunaal.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  8 19:50:18 2021
+
+@author: alterkait
+"""
+
+
+
+def name():
+    name = input("Hi. what's your name")
+
+    print("Hello", name)
